3909c9d9c1960111604322d541dee22e5abcc118
André Makrilou <andre@Andres-MBP-2.lan>
2020-05-28 15:20:36 +0100

    paypal button added

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import PaypalButton from './PayPalButton'
 
 
-export default function CartTotals({value}) {
+export default function CartTotals({value, history}) {
     const{cartSubTotal,cartTax,cartTotal,clearCart} = value;
     return (
         <React.Fragment>
@@ -23,7 +24,8 @@ export default function CartTotals({value}) {
                     <h5>
                         <span className="text-title">total:</span>
                         <strong>{cartTotal}€</strong>
-                    </h5>  
+                    </h5>
+                    <PaypalButton total={cartTotal} clearCart={clearCart} history={history}/>
                     </div>
                 </div>
             </div>
